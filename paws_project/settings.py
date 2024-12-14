6416a529bd120f98ef0812992407fa5a78560d22
assemahmed <amahmed89@outlook.com>
2024-12-14 11:50:41 -0800

    updating data

--- paws_project/settings.py
+++ paws_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'paws_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': "paws_rescue_db",
+        'USER': "root",
+        'PASSWORD': "123456",
+        'HOST': "localhost",
+        'PORT': "3306",
     }
 }
 
@@ -118,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/ "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
